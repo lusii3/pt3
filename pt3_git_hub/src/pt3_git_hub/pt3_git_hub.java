24204c86ffed28fe5bb413a8784a84386e13c434
dam1 <dam1@DAM1>
2024-02-02 10:36:35 +0100

    3a modificacio de Git

--- pt3_git_hub/src/pt3_git_hub/pt3_git_hub.java
+++ pt3_git_hub/src/pt3_git_hub/pt3_git_hub.java
@@ -4,5 +4,6 @@ public class pt3_git_hub {
 	public static void main(String[] args) {
 		System.out.println("la modificacio Git");
 		System.out.println("2a modificacio Git");
+		System.out.println("3a modificacio Git");
 	}
 }
