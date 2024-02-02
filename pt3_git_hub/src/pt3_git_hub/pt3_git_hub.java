93ea31678591c70f3a5f86b891b52a8e70bb8abd
lusii3 <157804604+lusii3@users.noreply.github.com>
2024-02-02 09:15:39 +0100

    2a modificacio del main

--- pt3_git_hub/src/pt3_git_hub/pt3_git_hub.java
+++ pt3_git_hub/src/pt3_git_hub/pt3_git_hub.java
@@ -3,5 +3,7 @@ package pt3_git_hub;
 public class pt3_git_hub {
 	public static void main(String[] args) {
 		System.out.println("la modificacio Git");
+		System.out.println("2a modificacio Git");
+
 	}
 }
