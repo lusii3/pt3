a78ed2f52aad84e11aa77a518ea4756324d32b57
dam1 <dam1@DAM1>
2024-02-02 11:11:09 +0100

    4a modificacio Git

--- pt3_git_hub/src/pt3_git_hub/pt3_git_hub.java
+++ pt3_git_hub/src/pt3_git_hub/pt3_git_hub.java
@@ -5,6 +5,6 @@ public class pt3_git_hub {
 		System.out.println("la modificacio Git");
 		System.out.println("2a modificacio Git");
 		System.out.println("3a modificacio Git");
-		System.out.println("a4 modificacio Git");
+		System.out.println("4a modificacio Git");
 	}	
 }
